afc44048eaadca0d750b111a13227f44415ded04
sean <seanxu24@outlook.com>
2022-05-19 18:16:29 +0800

    basic data type

--- src/main.rs
+++ src/main.rs
@@ -1,59 +1,94 @@
+extern crate core;
 
-fn greet_world(){
-    let southern_germany = "Grüß Gott!";
-    let chinese = "世界，你好";
-    let english = "World, hello";
-    let regions = [southern_germany,chinese,english];
-    for region in regions.iter() {
-        println!("{}",&region)
-    }
+use core::panicking::panic;
+use num::complex::Complex;
+use std::fmt::Debug;
+
+fn complex_num() {
+    let a = Complex { re: 2.1, im: -1.2 };
+    let b = Complex::new(11.1, 22.2);
+    let result = a + b;
+
+    println!("{}+{}i", result.re, result.im);
 }
 
-fn penguin(){
-    let penguin_data = "\
-    common name,length (cm)
-    lettle penguin,33
-    Yellow-eyed penguin,65
-    Fiordland penguin,60
-    Invalid,data
-    ";
+fn add_with_extra(x: i32, y: i32) -> i32 {
+    let x = x + 1;
+    let y = y + 1;
+    x + y
+}
 
-    let records = penguin_data.lines();
+fn ret_unit_type() {
+    let x = 1;
+    if (x > 1) {}
+}
 
-    for(i,record) in records.enumerate(){
-        if i==0 || record.trim().len() == 0 {
-            continue;
-        }
+fn add(i: i32, j: i32) -> i32 {
+    i + j
+}
 
-        // 声明一个 fields 变量，类型是 Vec
-        // Vec 是 vector 的缩写，是一个可伸缩的集合类型，可以认为是一个动态数组
-        // <_>表示 Vec 的元素类型是由编译器自动推断的，在很多场景下，都会帮我们省却不少功夫
-        let fields:Vec<_> = record
-            .split(',')
-            .map(|field| field.trim())
-            .collect();
-        if cfg!(debug_assertions){
-            // 输出到标准错误输出
-            eprintln!("debug:{:?} -> {:?}",record,fields)
-        }
+fn another_function(x: i32, y: f32) {
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+}
 
-        let name = fields[0];
-        // 1。 尝试把 fields[1] 的值转换为f32 类型的浮点数，如果成功，则把f32值赋给 length 变量
-        //
-        // 2. if let 是一个匹配表达式，用来从=右边的结果中，匹配出 length 的值：
-        //   1) 当=右边的表达式执行成功，则会返回一个OK(f32)的类型，若失败，则会返回一个 Err(e) 类型， if let 的作用就是仅匹配 OK 也就是成功的情况，如果是错误，就直接忽略
-        //   2) 同时 if let 还会做一次解构匹配，通过 OK(length)去匹配右边的Ok(f32)，最终把相应的f32值赋给length
-        //
-        // 3. 当然你也可以忽略成功的情况，用 if let Err(e) = fields[1].parse::<f32>() {...}匹配出错误，然后打印出来，但是没啥卵用
-        if let Ok(length) = fields[1].parse::<f32>(){
-            // 输出到标准输出
-            println!("{},{}cm",name,length)
-        }
+fn plus_five(x: i32) -> i32 {
+    x + 5
+}
+
+fn plus_or_minus(x: i32) -> i32 {
+    if x > 5 {
+        return x - 5
     }
+
+    x + 5
+}
+
+fn report<T: Debug>(item: T) {
+    println!("{:?}", item);
+}
+
+fn clear(text: &mut String) -> () {
+    *text = String::from("");
+}
+
+fn add1(x: u32, y: u32) -> u32 {
+    x + y
+}
+
+fn dead_end()->!{
+    panic!("崩溃吧！");
+}
+
+fn forever()->!{
+    loop{
+        //
+    };
 }
 
 fn main() {
-    println!("Hello, world!");
-    greet_world();
-    penguin();
+    //complex_num();
+    let x = '中';
+    println!("字符'中'占用了{}字节的内存大小", std::mem::size_of_val(&x));
+
+    //add_with_extra(1,2);
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    println!("The value of y is:{}", y);
+
+    assert_eq!(ret_unit_type(), ());
+
+    another_function(5, 6.1);
+
+    let x = plus_five(5);
+    println!("The value of x is: {}", x);
+
+    let x = plus_or_minus(5);
+    println!("The value of x is: {}", x);
+
+    report("test");
+    dead_end();
 }
