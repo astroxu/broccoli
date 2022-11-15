63b49c98fd92d5b48281b781d41c0d694686257a
xcg <xuchengang@uniondrug.com>
2022-11-15 18:19:45 +0800

    axum postgres

--- try-axum/postgres/src/main.rs
+++ try-axum/postgres/src/main.rs
@@ -1,3 +1,22 @@
+use axum::{extract::Path, routing, Router};
+use tokio_pg_mapper::FromTokioPostgresRow;
+use tokio_pg_mapper_derive::PostgresMapper;
+
+#[derive(PostgresMapper, Debug)]
+#[pg_mapper(table = "account")]
+pub struct Account {
+    pub id: i32,
+    pub username: String,
+    pub balance: i32,
+}
+
+pub struct CreateAccount {
+    pub username: String,
+    pub balance: i32,
+}
+
+
+
 fn main() {
     println!("Hello, world!");
 }
